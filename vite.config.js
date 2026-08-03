4f9a20b46ce3a9d379ed63e0dae7b10533ea0b9c
sebasss18 <sebasbernals11@gmail.com>
2026-08-03 14:11:49 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
-  publicDir: "public",
+  publicDir: "../public",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        planets: "src/planets/index.html",
+        favorites: "src/favorites/index.html",
+        apod: "src/apod/index.html",
+      },
+    },
   },
 });
